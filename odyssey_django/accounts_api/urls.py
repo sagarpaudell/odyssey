67d71d84867b78f23b57c89cd830125d805b9583
buddha gautam <buddhagautam231@gmail.com>
2021-06-07 13:36:09 +0545

    Server side code added

---
+++ odyssey_django/accounts_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.authtoken import views
+from .views import UserRecordView
+
+app_name='accounts_api'
+urlpatterns = [
+        # make post request to user to register
+        # make get request with 'Key token' in authorization header to get the user info
+        path('user/', UserRecordView.as_view(), name='user'),
+        # make post request to api-get-auth to get the token of the user
+        path('get-auth-token/', views.obtain_auth_token, name='api-token-auth'),
+    ]
---
+++ odyssey_django/accounts_api/views.py
@@ -0,0 +1,36 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+from .serializers import UserSerializer
+
+
+class UserRecordView(APIView):
+    """ API View to create or get user info.
+    a POST request allows to create a new user.
+    """
+    def get(self, format=None):
+        """make get request with token present in the authentication header
+        to get user info"""
+        user = Token.objects.get(key=self.get_authenticate_header).user
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer)
+
+    def post(self, request):
+        """make post request with user info to register"""
+        serializer = UserSerializer(data = request.data)
+        print(serializer)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.create(validated_data=request.data)
+            return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED,
+                )
+        return Response(
+               {
+                   "error":True,
+                   "error_msg": serializer.error_messages,
+               },
+               status=status.HTTP_400_BAD_REQUEST
+               )
