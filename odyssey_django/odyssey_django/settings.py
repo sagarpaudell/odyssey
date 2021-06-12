195f34c8ba74f46111360fb136a3a9cd949324c9
buddha gautam <buddhagautam231@gmail.com>
2021-06-12 20:45:15 +0545

    csrf fix heroku

--- odyssey_django/odyssey_django/settings.py
+++ odyssey_django/odyssey_django/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
