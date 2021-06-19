50a6dce2f1c3f91045ec51a9d260e596e820dd8b
Biraj Adhikari <birajkmc4@gmail.com>
2021-06-19 20:57:52 +0545

    Added Places Model and API endpoints

--- odyssey_django/odyssey_django/settings.py
+++ odyssey_django/odyssey_django/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'accounts_api',
     'traveller_api',
+    'places_api',
 ]
 
 REST_FRAMEWORK = {
--- odyssey_django/odyssey_django/urls.py
+++ odyssey_django/odyssey_django/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('accounts-api/', include('accounts_api.urls', namespace='api')),
-    path('traveller-api/', include('traveller_api.urls'))
+    path('traveller-api/', include('traveller_api.urls')),
+    path('places-api/', include('places_api.urls')),
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
