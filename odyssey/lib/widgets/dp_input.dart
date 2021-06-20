43ff018232f24538f4b54502d4cca28d058ef0c3
sagarpaudell <sp02434118@student.ku.edu.np>
2021-06-20 10:12:24 +0545

    android gradle files added

--- odyssey/lib/widgets/dp_input.dart
+++ odyssey/lib/widgets/dp_input.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DpInput extends StatefulWidget {
   Function onSelectImg;
@@ -39,33 +40,35 @@ class _DpInputState extends State<DpInput> {
     return Container(
       width: deviceSize.width * 0.45,
       child: Stack(
-        alignment: Alignment.bottomCenter,
+        fit: StackFit.expand,
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.grey),
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: _storedImage != null
-                  ? Image.file(
-                      _storedImage,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                    )
-                  : Image.asset(
-                      './assets/images/default_dp.png',
-                    ),
-              alignment: Alignment.center,
-            ),
+          CircleAvatar(
+            // ClipRRect(
+            //   borderRadius: BorderRadius.circular(100),
+            //   child: Container(
+            //     width: 150,
+            //     height: 150,
+            //     decoration: BoxDecoration(
+            //       border: Border.all(width: 1, color: Colors.grey),
+            //       borderRadius: BorderRadius.circular(100),
+            //     ),
+            child: _storedImage != null
+                ? Image.file(
+                    _storedImage,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  )
+                : Image.asset(
+                    './assets/images/default_dp.png',
+                  ),
           ),
-          IconButton(
-            icon: Icon(Icons.camera),
-            onPressed: _takePicture,
-          ),
+          TextButton(
+              onPressed: _takePicture,
+              child: SvgPicture.asset("assets/icons/Camera Icon.svg"))
+          // IconButton(
+          //   icon: Icon(Icons.camera),
+          //   onPressed: _takePicture,
+          // ),
         ],
       ),
     );
