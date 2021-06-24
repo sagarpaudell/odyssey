998f264d3eb4112d3f433a6a3a896476f59cb57a
buddha gautam <buddhagautam231@gmail.com>
2021-06-24 14:26:01 +0545

    configured asgi for use with daphne

--- odyssey_django/odyssey_django/asgi.py
+++ odyssey_django/odyssey_django/asgi.py
@@ -1,14 +1,16 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odyssey_django.settings')
+django_asgi_app = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 from .channelsmiddleware import TokenAuthMiddleware
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': TokenAuthMiddleware(
         URLRouter(
             chat.routing.websocket_urlpatterns
