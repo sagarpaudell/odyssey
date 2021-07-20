6bca3b9e3ccd7b94833c443adea8b117539c1d9c
Biraj Adhikari <birajkmc4@gmail.com>
2021-07-20 13:18:01 +0545

    API endpoint for Blog Comment View

--- odyssey_django/traveller_api/serializers.py
+++ odyssey_django/traveller_api/serializers.py
@@ -8,3 +8,10 @@ class TravellerSerializer(serializers.ModelSerializer):
         model = Traveller
         fields = ['username', 'id', 'first_name', 'last_name', 'address', 
                 'city', 'country', 'bio', 'contact_no', 'gender', 'photo_main']
+
+class TravellerSerializerPublic(serializers.ModelSerializer):
+    username = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    # following = serializers.StringRelatedField(many = True)
+    class Meta:
+        model = Traveller
+        fields = ['username', 'id', 'first_name', 'last_name', 'photo_main']
