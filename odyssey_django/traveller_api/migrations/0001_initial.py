a7e1e7c74e34d5279723632ac353caeecc1f4b86
buddha gautam <buddhagautam231@gmail.com>
2021-07-20 20:14:45 +0545

    test heroku

--- odyssey_django/traveller_api/migrations/0001_initial.py
+++ odyssey_django/traveller_api/migrations/0001_initial.py
@@ -14,23 +14,23 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Traveller',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(blank=True, max_length=200)),
-                ('last_name', models.CharField(blank=True, max_length=200)),
-                ('address', models.CharField(blank=True, max_length=200)),
-                ('city', models.CharField(blank=True, max_length=100)),
-                ('country', models.CharField(blank=True, max_length=100)),
-                ('bio', models.TextField(blank=True)),
-                ('contact_no', models.CharField(blank=True, max_length=20)),
-                ('gender', models.CharField(blank=True, max_length=10)),
-                ('photo_main', models.ImageField(blank=True, upload_to='profile_photos/%Y/%m/%d/')),
-                ('reg_date', models.DateTimeField(auto_now_add=True)),
-                ('username', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
+        # migrations.CreateModel(
+            # name='Traveller',
+            # fields=[
+                # ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                # ('first_name', models.CharField(blank=True, max_length=200)),
+                # ('last_name', models.CharField(blank=True, max_length=200)),
+                # ('address', models.CharField(blank=True, max_length=200)),
+                # ('city', models.CharField(blank=True, max_length=100)),
+                # ('country', models.CharField(blank=True, max_length=100)),
+                # ('bio', models.TextField(blank=True)),
+                # ('contact_no', models.CharField(blank=True, max_length=20)),
+                # ('gender', models.CharField(blank=True, max_length=10)),
+                # ('photo_main', models.ImageField(blank=True, upload_to='profile_photos/%Y/%m/%d/')),
+                # ('reg_date', models.DateTimeField(auto_now_add=True)),
+                # ('username', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            # ],
+        # ),
         migrations.CreateModel(
             name='TravellerFollowing',
             fields=[
