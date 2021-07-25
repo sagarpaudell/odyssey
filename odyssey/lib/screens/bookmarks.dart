5ee0eea52e5c043b336d312a4e2291ee55dc7b1d
sagarpaudell <sp02434118@student.ku.edu.np>
2021-07-25 23:48:31 +0545

    bookmark feature added

--- odyssey/lib/screens/bookmarks.dart
+++ odyssey/lib/screens/bookmarks.dart
@@ -1,45 +1,161 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../providers/posts.dart';
+import '../providers/blog.dart' as blogss;
+import 'package:provider/provider.dart';
+import '../widgets/fb_loading.dart';
+import '../widgets/post_container.dart';
+import '../widgets/blog_container.dart';
 
-class Bookmark extends StatelessWidget {
+class Bookmark extends StatefulWidget {
+  @override
+  _BookmarkState createState() => _BookmarkState();
+}
+
+class _BookmarkState extends State<Bookmark> {
+  List<bool> isSelected = [true, false];
+  bool isPosts = true;
+  List<dynamic> bookmarkedPosts;
+  List<dynamic> bookmarkedBlogs;
+  void refreshBookmark() {
+    initState();
+  }
+
+  Future fbuilder;
+  @override
+  void initState() {
+    fbuilder = getBPosts();
+    super.initState();
+  }
+
+  Future<void> getBPosts() async {
+    var temp =
+        await Provider.of<Posts>(context, listen: false).getBookmarkedPosts();
+    setState(() {
+      bookmarkedPosts = temp;
+    });
+  }
+
+  Future<void> getBookmarkedBlogs() async {
+    List<dynamic> tempblogs =
+        await Provider.of<blogss.Blog>(context, listen: false)
+            .getBookmarkedBlogs();
+    setState(() {
+      bookmarkedBlogs = tempblogs;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!isPosts) {
+      getBookmarkedBlogs();
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Bookmarks'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("You haven't bookmarked any posts."),
-                ],
-              ),
-            ),
-          ),
-          // Row(
-          //   children: [
-          //     Expanded(
-          //       child: Padding(
-          //         padding: const EdgeInsets.all(12.0),
-          //         child: TextField(
-          //           decoration: InputDecoration(
-          //             hintText: 'Write a comment',
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //     IconButton(
-          //       icon: Icon(Icons.send),
-          //       onPressed: null,
-          //     )
-          //   ],
-          // ),
-        ],
+      body: FutureBuilder<void>(
+        future: fbuilder, // a previously-obtained Future<String> or null
+        builder: (BuildContext context, AsyncSnapshot<void> snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? SingleChildScrollView(child: FbLoading())
+                : bookmarkedPosts.isEmpty
+                    ? Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("You haven't bookmarked any posts."),
+                          ],
+                        ),
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(
+                              child: ToggleButtons(
+                                children: [
+                                  Text(
+                                    'Posts',
+                                  ),
+                                  Text('Blogs')
+                                ],
+                                isSelected: isSelected,
+                                selectedColor: Colors.white,
+                                splashColor: Colors.lightBlue,
+                                fillColor: Theme.of(context).primaryColor,
+                                onPressed: (int index) {
+                                  setState(() {
+                                    for (int indexBtn = 0;
+                                        indexBtn < isSelected.length;
+                                        indexBtn++) {
+                                      if (indexBtn == index) {
+                                        isSelected[indexBtn] =
+                                            !isSelected[indexBtn];
+                                      } else {
+                                        isSelected[indexBtn] = false;
+                                      }
+                                    }
+                                    isPosts = !isPosts;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: isPosts
+                                ? ListView.builder(
+                                    itemBuilder: (context, index) {
+                                      return PostContainer(
+                                          post: bookmarkedPosts[index],
+                                          fun: refreshBookmark);
+                                    },
+                                    itemCount: bookmarkedPosts.length,
+                                  )
+                                : bookmarkedBlogs == null
+                                    ? SingleChildScrollView(child: FbLoading())
+                                    : bookmarkedBlogs.isEmpty
+                                        ? Center(
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text(
+                                                    "You haven't bookmarked any posts."),
+                                              ],
+                                            ),
+                                          )
+                                        : ListView.builder(
+                                            itemBuilder: (context, index) {
+                                              return BlogContainer(
+                                                  bookmarkedBlogs[index]);
+                                            },
+                                            itemCount: bookmarkedBlogs.length,
+                                          ),
+                          ),
+                        ],
+
+                        // Row(
+                        //   children: [
+                        //     Expanded(
+                        //       child: Padding(
+                        //         padding: const EdgeInsets.all(12.0),
+                        //         child: TextField(
+                        //           decoration: InputDecoration(
+                        //             hintText: 'Write a comment',
+                        //           ),
+                        //         ),
+                        //       ),
+                        //     ),
+                        //     IconButton(
+                        //       icon: Icon(Icons.send),
+                        //       onPressed: null,
+                        //     )
+                        //   ],
+                        // ),
+                      ),
       ),
     );
   }
--- odyssey/lib/widgets/post_container.dart
+++ odyssey/lib/widgets/post_container.dart
@@ -5,6 +5,7 @@ import 'package:focused_menu/focused_menu.dart';
 import 'package:focused_menu/modals.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:odyssey/models/models.dart';
+import 'package:odyssey/providers/posts.dart';
 import 'package:odyssey/screens/comment_post.dart';
 import 'package:odyssey/widgets/profile_avatar.dart';
 import 'package:intl/intl.dart';
@@ -14,16 +15,39 @@ import '../screens/profile_user.dart';
 import '../providers/auth.dart';
 import 'package:provider/provider.dart';
 
-class PostContainer extends StatelessWidget {
+class PostContainer extends StatefulWidget {
   final Map<String, dynamic> post;
+  final Function fun;
+  const PostContainer({Key key, @required this.post, this.fun})
+      : super(key: key);
 
-  const PostContainer({
-    Key key,
-    @required this.post,
-  }) : super(key: key);
+  @override
+  _PostContainerState createState() => _PostContainerState();
+}
+
+class _PostContainerState extends State<PostContainer> {
+  bool _is_bookmarked = false;
+  void toggleBookmark() {
+    if (widget.fun != null) {
+      widget.fun();
+    }
+    setState(() {
+      _is_bookmarked = !_is_bookmarked;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (widget.post['is_bookmarked']) {
+      _is_bookmarked = true;
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final String selfUserName = Provider.of<Auth>(context).userName;
+
     return Card(
       shadowColor: Colors.white,
       margin: EdgeInsets.symmetric(
@@ -39,21 +63,21 @@ class PostContainer extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  _PostHeader(post: post),
+                  _PostHeader(widget.post, selfUserName, context),
                   const SizedBox(height: 4.0),
-                  Text(post['caption']),
-                  post['photo_main'] != null
+                  Text(widget.post['caption']),
+                  widget.post['photo_main'] != null
                       ? const SizedBox.shrink()
                       : const SizedBox(height: 14.0),
                 ],
               ),
             ),
-            post['photo'] != null
+            widget.post['photo'] != null
                 ? Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: Image(
                       image: NetworkImage(
-                        post["photo"],
+                        widget.post["photo"],
                       ),
                       errorBuilder: (BuildContext context, Object exception,
                           StackTrace stackTrace) {
@@ -64,7 +88,8 @@ class PostContainer extends StatelessWidget {
                 : const SizedBox.shrink(),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: _PostButtons(post: post),
+              child: _PostButtons(
+                  widget.post, context, toggleBookmark, _is_bookmarked),
             ),
           ],
         ),
@@ -73,170 +98,173 @@ class PostContainer extends StatelessWidget {
   }
 }
 
-class _PostHeader extends StatelessWidget {
-  final Map<String, dynamic> post;
+// class _PostHeader extends StatelessWidget {
+//   final Map<String, dynamic> post;
 
-  const _PostHeader({
-    Key key,
-    @required this.post,
-  }) : super(key: key);
+//   const _PostHeader({
+//     Key key,
+//     @required this.post,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final String selfUserName = Provider.of<Auth>(context).userName;
-
-    return Row(
-      children: [
-        GestureDetector(
-          onTap: () {
-            if (post['traveller']['username'] == selfUserName) {
-              Navigator.of(context).pushReplacementNamed(SelfProfile.routeName);
-            } else {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => UserProfile(post['traveller']['username']),
-                ),
-              );
-            }
-          },
-          child: ProfileAvatar(imageUrl: post['traveller']['photo_main']),
-        ),
-        const SizedBox(width: 8.0),
-        Expanded(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '${post['traveller']['username']} • ',
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                ),
+//   @override
+//   Widget build(BuildContext context) {
+Widget _PostHeader(
+    Map<String, dynamic> post, String selfUserName, BuildContext context) {
+  return Row(
+    children: [
+      GestureDetector(
+        onTap: () {
+          if (post['traveller']['username'] == selfUserName) {
+            Navigator.of(context).pushReplacementNamed(SelfProfile.routeName);
+          } else {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => UserProfile(post['traveller']['username']),
               ),
-              Jiffy(DateTime.parse(post['post_time']))
-                      .fromNow()
-                      .toString()
-                      .contains(RegExp(r'hours|minutes|seconds'))
-                  ? Text(
-                      Jiffy(DateTime.parse(post['post_time'])).fromNow(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                      ),
-                    )
-                  : Text(
-                      DateFormat('MMM dd, yyyy')
-                          .format(DateTime.parse(post['post_time'])),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                      ),
-                    ),
-            ],
-          ),
-        ),
-        FocusedMenuHolder(
-          openWithTap: true,
-          onPressed: () {},
-          menuItems: [
-            FocusedMenuItem(
-              title: Text('Delete Post'),
-              trailingIcon: Icon(Icons.delete),
-              onPressed: () {},
+            );
+          }
+        },
+        child: ProfileAvatar(imageUrl: post['traveller']['photo_main']),
+      ),
+      const SizedBox(width: 8.0),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${post['traveller']['username']} • ',
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
             ),
+            Jiffy(DateTime.parse(post['post_time']))
+                    .fromNow()
+                    .toString()
+                    .contains(RegExp(r'hours|minutes|seconds'))
+                ? Text(
+                    Jiffy(DateTime.parse(post['post_time'])).fromNow(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),
+                  )
+                : Text(
+                    DateFormat('MMM dd, yyyy')
+                        .format(DateTime.parse(post['post_time'])),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),
+                  ),
           ],
-          child: IconButton(
-            icon: const Icon(Icons.more_horiz),
-            onPressed: () => print('hello'),
-          ),
         ),
-      ],
-    );
-  }
+      ),
+      FocusedMenuHolder(
+        openWithTap: true,
+        onPressed: () {},
+        menuItems: [
+          FocusedMenuItem(
+            title: Text('Delete Post'),
+            trailingIcon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ],
+        child: IconButton(
+          icon: const Icon(Icons.more_horiz),
+          onPressed: () => print('hello'),
+        ),
+      ),
+    ],
+  );
 }
 
-class _PostButtons extends StatelessWidget {
-  final Map<String, dynamic> post;
+Widget _PostButtons(Map<String, dynamic> post, BuildContext context,
+    Function toggleB, bool _is_bookmarked) {
+  return Column(
+    children: [
+      const SizedBox(height: 8.0),
 
-  const _PostButtons({
-    Key key,
-    @required this.post,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 8.0),
-
-        Row(
-          children: [
-            const Icon(
-              Icons.emoji_emotions_outlined,
-              size: 24.0,
+      Row(
+        children: [
+          const Icon(
+            Icons.emoji_emotions_outlined,
+            size: 24.0,
+          ),
+          const SizedBox(width: 4.0),
+          Text(
+            '${post["like_users"].length}',
+            style: TextStyle(
+              color: Colors.grey[600],
             ),
-            const SizedBox(width: 4.0),
-            Text(
-              '${post["like_users"].length}',
-              style: TextStyle(
-                color: Colors.grey[600],
+          ),
+          const SizedBox(width: 8.0),
+          GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => Comment(),
               ),
             ),
-            const SizedBox(width: 8.0),
-            GestureDetector(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => Comment(),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.comment_outlined,
+                  size: 24.0,
                 ),
-              ),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.comment_outlined,
-                    size: 24.0,
+                const SizedBox(width: 4.0),
+                Text(
+                  '${post["comments"].length}',
+                  style: TextStyle(
+                    color: Colors.grey[600],
                   ),
-                  const SizedBox(width: 4.0),
-                  Text(
-                    '${post["comments"].length}',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const SizedBox(width: 8.0),
-            const Icon(
-              Icons.share_outlined,
-              size: 24.0,
+          ),
+          const SizedBox(width: 8.0),
+          const Icon(
+            Icons.share_outlined,
+            size: 24.0,
+          ),
+          const SizedBox(width: 4.0),
+          Text(
+            '0',
+            style: TextStyle(
+              color: Colors.grey[600],
             ),
-            const SizedBox(width: 4.0),
-            Text(
-              '0',
-              style: TextStyle(
-                color: Colors.grey[600],
-              ),
+          ),
+          const SizedBox(width: 8.0),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Provider.of<Posts>(context, listen: false)
+                        .toogleBookmarkedPost(post['id'].toString());
+                    toggleB();
+                  },
+                  icon: _is_bookmarked
+                      ? Icon(
+                          Icons.bookmark,
+                          color: Theme.of(context).primaryColor,
+                          size: 24,
+                        )
+                      : Icon(
+                          Icons.bookmark_border_outlined,
+                          size: 24,
+                        ),
+                ),
+              ],
             ),
-            const SizedBox(width: 8.0),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Icon(
-                    Icons.bookmark_border_outlined,
-                    size: 24,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(width: 4.0),
-          ],
-        ),
-        // const Divider(),
-        const SizedBox(height: 12.0),
-      ],
-    );
-  }
+          ),
+          const SizedBox(width: 4.0),
+        ],
+      ),
+      // const Divider(),
+      const SizedBox(height: 12.0),
+    ],
+  );
 }
