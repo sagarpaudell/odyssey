35f4e4889a91c3a455d447c9930fa2ce543e7110
sagarpaudell <sp02434118@student.ku.edu.np>
2021-08-07 13:49:02 +0545

    places  render

--- odyssey/lib/widgets/blog_container.dart
+++ odyssey/lib/widgets/blog_container.dart
@@ -88,7 +88,7 @@ class _BlogContainerState extends State<BlogContainer> {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             Icon(Icons.location_on_outlined),
-                            Text('Langtang'),
+                            Text(widget.singleBlog['place']['name']),
                           ],
                         ),
                       ),
