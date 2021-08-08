0f189fd3820cdba2f6bcc4f86929f7d9f719ba06
Samesh <mir.rooshaan@gmail.com>
2021-08-08 17:58:04 +0545

    explore places page, single place page and create new place field

--- odyssey/lib/widgets/place_container.dart
+++ odyssey/lib/widgets/place_container.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import '../screens/place_screen.dart';
 import '../providers/auth.dart';
 import 'package:provider/provider.dart';
 // import '../screens/single_Place_screen.dart';
@@ -16,10 +18,21 @@ class PlaceContainer extends StatefulWidget {
 class _PlaceContainerState extends State<PlaceContainer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: () {
+        print('hi');
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => PlaceScreen(),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.only(bottom: 10),
+        height: 300,
+        decoration: BoxDecoration(
           border: Border.all(
             color: Colors.grey[200],
             width: 3,
@@ -28,27 +41,55 @@ class _PlaceContainerState extends State<PlaceContainer> {
           //   // blurRadius: ,
 
           // )],
-          borderRadius: BorderRadius.all(Radius.circular(30))),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15),
-        child: Column(
-          children: [
-            Image(
-              image: NetworkImage(widget.singlePlace["photo1"]),
-              errorBuilder: (BuildContext context, Object exception,
-                  StackTrace stackTrace) {
-                return Image.asset('./assets/images/mana.jpg');
-              },
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: _PlaceInfo(
-                widget.singlePlace,
-                context,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Column(
+            children: [
+              // Image(
+              //   image: NetworkImage(widget.singlePlace["photo1"]),
+              //   errorBuilder: (BuildContext context, Object exception,
+              //       StackTrace stackTrace) {
+              //     return Image.asset('./assets/images/mana.jpg');
+              //   },
+              //   height: 250,
+              //   width: double.infinity,
+              //   fit: BoxFit.cover,
+              // ),
+
+              ImageSlideshow(
+                width: double.infinity,
+                height: 250,
+                initialPage: 0,
+                indicatorColor: Theme.of(context).primaryColor,
+                indicatorBackgroundColor: Colors.grey[600],
+                autoPlayInterval: 0,
+                isLoop: true,
+                children: [
+                  Image.network(
+                    'https://source.unsplash.com/random/300x300',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://source.unsplash.com/random/300x300',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://source.unsplash.com/random/300x300',
+                    fit: BoxFit.cover,
+                  ),
+                ],
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _PlaceInfo(
+                  widget.singlePlace,
+                  context,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -67,10 +108,11 @@ Widget _PlaceInfo(
       Text(
         singlePlace['title'],
         style: TextStyle(
-            fontFamily: 'Arial',
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.8),
+          fontFamily: 'Arial',
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 0.8,
+        ),
       ),
     ],
 
