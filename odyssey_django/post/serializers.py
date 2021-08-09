678c8deff12c74eb0f89dd3a9d67b656ddd6ae5d
buddha gautam <buddhagautam231@gmail.com>
2021-08-09 17:13:25 +0545

    fix: place model

--- odyssey_django/post/serializers.py
+++ odyssey_django/post/serializers.py
@@ -33,19 +33,6 @@ class PostSerializer(ModelSerializer):
             return obj.is_bookmarked(traveller)
         return False
 
-
-
-
-    # def create(self, validated_data, traveller, place):
-        # """ create new posts """
-        # print (validated_data)
-        # photo = validated_data.pop("photo")
-        # post = Post.objects.create(photo = photo[0], traveller = traveller,
-                # place = place, **validated_data)
-        # post.save()
-        # print(post)
-        # return PostSerializer(post)
-
     class Meta:
         model = Post
         fields=("id", "traveller", "caption", "photo", "place", "like_users",
