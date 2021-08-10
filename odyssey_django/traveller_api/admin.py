fa4f11710918dccbe6a49f52c73e0dfd51e566a9
buddha gautam <buddhagautam231@gmail.com>
2021-08-10 20:29:28 +0545

    feat: traveller following filter admin panel

--- odyssey_django/traveller_api/admin.py
+++ odyssey_django/traveller_api/admin.py
@@ -8,6 +8,7 @@ class TravellerAdmin(admin.ModelAdmin):
 class TravellerFollowingAdmin(admin.ModelAdmin):
     list_display = ('traveller_id','following_traveller_id')
     list_display_links = ('traveller_id', 'following_traveller_id')
+    list_filter = ('traveller_id','following_traveller_id' )
 
 admin.site.register(Traveller, TravellerAdmin)
 admin.site.register(TravellerFollowing, TravellerFollowingAdmin)
