b8d9d8e22664e04d7e7aecc98b9b7527a1d0a542
buddha gautam <buddhagautam231@gmail.com>
2021-08-10 22:18:00 +0545

    feat: end point to retrieve follower following of other users

--- odyssey_django/traveller_api/urls.py
+++ odyssey_django/traveller_api/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import (
-        TravellerGetView, TravellerView, FollowView, GetFollowers,
-        GetFollowing, SearchTraveller
+        TravellerGetView, TravellerView, FollowView, GetUserFollowers,
+        GetUserFollowing, SearchTraveller, GetSelfFollowers, GetSelfFollowing
     )
 
 
 urlpatterns = [
     path('', TravellerView.as_view()),
-    path('followers', GetFollowers.as_view()),
-    path('following', GetFollowing.as_view()),
+    path('followers', GetSelfFollowers.as_view()),
+    path('following', GetSelfFollowing.as_view()),
+    path('followers/<str:username>', GetUserFollowers.as_view()),
+    path('following/<str:username>', GetUserFollowing.as_view()),
     path('follow-user/<str:username>', FollowView.as_view()),
     path('search', SearchTraveller.as_view()),
     path('<str:username>', TravellerGetView.as_view()),
